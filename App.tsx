f27ab42550e1b9bc3c463673dbbba8cbdda568a3
Lívia <liviabelirocha@outlook.com>
2021-06-22 09:31:43 -0300

    Adding fonts, theme, and illustrations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import {Text, View } from 'react-native';
+import React from "react";
+import { Text, StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import {
+  Rubik_300Light,
+  Rubik_400Regular,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
+import {
+  Nunito_400Regular,
+  Nunito_600SemiBold,
+} from "@expo-google-fonts/nunito";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Rubik_300Light,
+    Rubik_400Regular,
+    Rubik_700Bold,
+    Nunito_400Regular,
+    Nunito_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
-    <View>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Text>Plant Daddy</Text>
-    </View>
+    </>
   );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primaryWhite: "#F2F2F2",
+    lightGreen: "#65CCB7",
+    mediumGreen: "#327F6B",
+    darkGreen: "#124647",
+  },
+  fonts: {
+    rubik300: "Rubik_300Light",
+    rubik400: "Rubik_400Regular",
+    rubik700: "Rubik_700Bold",
+    nunito400: "Nunito_400Regular",
+    nunito600: "Nunito_600SemiBold",
+  },
+};
